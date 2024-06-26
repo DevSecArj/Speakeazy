ebed747360ff904200d08c801cc2668ec67a898b
Arjun Srinivas Nayak <arjunsriniv@vmware.com>
2024-06-26 23:36:46 +0530

    Body with Bg

---
+++ speakeazy/src/Body.js
@@ -0,0 +1,11 @@
+function Body()
+{    return (
+        <div className="w-screen h-screen bg-blue-500">
+            <div className="bg-white h-3/6">
+
+            </div>
+        </div>
+    )
+}
+
+export default Body
